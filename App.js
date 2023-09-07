1777f6a77e356cd1608c5af7bcfd874c94a2a509
Rishabh Prasad <rishabhprasad567@gmail.com>
2023-09-07 22:18:09 +0530

    "firstlecture"

---
+++ App.js
@@ -0,0 +1,28 @@
+const heading = React.createElement(
+    "h1",
+    {
+        id:"title"
+    },
+    "heading 1"
+);
+
+const heading2 = React.createElement(
+    "h2",
+    {
+        id:"title"
+    },
+    "heading 2"
+);
+
+const container = React.createElement(
+    "div",
+    {
+        id:"container"
+    },
+    [heading, heading2]
+);
+
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
