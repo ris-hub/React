83bd5992348b1d54a5b94612f98455bf4af5d70c
Rishabh Prasad <rishabhprasad567@gmail.com>
2023-09-09 17:46:34 +0530

    "second lecture"

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement(
     "h1",
     {
         id:"title"
     },
-    "heading 1"
+    "heading 1 for parcel"
 );
 
 const heading2 = React.createElement(
