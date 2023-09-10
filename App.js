72370bc414ec664c0c50a92db666d48a367f8091
Rishabh Prasad <rishabhprasad567@gmail.com>
2023-09-10 11:07:52 +0530

    third lecture(babel, jsx, components)

--- App.js
+++ App.js
@@ -3,15 +3,17 @@ import ReactDOM from "react-dom/client";
 const heading = React.createElement(
     "h1",
     {
-        id:"title"
+        id:"title",
+        key:"h1"
     },
-    "heading 1 for parcel"
+    "Namaste React"
 );
 
 const heading2 = React.createElement(
     "h2",
     {
-        id:"title"
+        id:"title",
+        key:"h2"
     },
     "heading 2"
 );
@@ -26,5 +28,49 @@ const container = React.createElement(
 
 console.log(heading);
 
+//functional component
+const Title = () =>(
+    <h1 id="title" key="h3">
+        Namaste React
+    </h1>
+);
+
+//functional component is nothing but a normal function in js.
+//here we are taking an example of arrow function.
+const HeaderComponent = () => {
+    return <h1>React Functional Component</h1>;
+}
+
+//writing multiple declarations but in single line
+const HeaderComponent2 = () => {
+    return <div><h1>React Functional Component</h1><h2>enjoy coding</h2></div>;
+}
+
+//writing multiple declarations but in separate line
+const HeaderComponent3 = () => {
+    return (
+        <div>
+            {Title()}
+            <h1>React Functional Component</h1>
+            <h2>enjoy coding</h2>
+        </div>
+    );
+}
+
+//normal function example
+const HeaderComponent4 = function() {
+    return (
+        <div>
+            <h1>React Functional Component</h1>
+            <h2>enjoy coding</h2>
+        </div>
+    );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+
+//rendering react component
+//root.render(container);
+
+//rendering functional component
+root.render(<HeaderComponent3/>);
