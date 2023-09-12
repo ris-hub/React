b19ff528c3fdc668e4b442b18f6aedb0269fa7a6
Rishabh Prasad <rishabhprasad567@gmail.com>
2023-09-12 19:01:42 +0530

    fifth lecture

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM, { createRoot } from "react-dom/client";
+import Header from "./components/Header"
+import Body from "./components/Body"
+import Footer from "./components/Footer"
+
+/**
+     Header
+        - Logo(Title)
+        - Nav Items(Right Side)
+        - Cart
+     Body 
+        - Search bar
+        - RestrauntList
+          - RestaurantCard (many cards)
+              - Image
+              - Name
+              - Rating
+              - Cusines
+     Footer
+      - links
+      - Copyright
+    
+    */
+
+//Config Driven UI
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
